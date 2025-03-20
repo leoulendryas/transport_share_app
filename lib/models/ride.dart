47345bb0de8494e9f20221d2e8aae09299ef148d
leoulendryas <leoulendryas@gmail.com>
2025-03-20 22:08:41 +0300

    Add Ride model with fromJson and toJson methods

---
+++ lib/models/ride.dart
@@ -0,0 +1,33 @@
+class Ride {
+  final String from;
+  final String to;
+  final int seats;
+  final DateTime time;
+
+  Ride({
+    required this.from,
+    required this.to,
+    required this.seats,
+    required this.time,
+  });
+
+  // Factory constructor to create a Ride object from JSON
+  factory Ride.fromJson(Map<String, dynamic> json) {
+    return Ride(
+      from: json['from'],
+      to: json['to'],
+      seats: json['seats'],
+      time: DateTime.parse(json['time']),
+    );
+  }
+
+  // Method to convert a Ride object to JSON
+  Map<String, dynamic> toJson() {
+    return {
+      'from': from,
+      'to': to,
+      'seats': seats,
+      'time': time.toIso8601String(),
+    };
+  }
+}
