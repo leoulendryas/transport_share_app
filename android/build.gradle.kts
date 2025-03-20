9f8783ce4848a2a1796a9afdcbb745540b1ac5bf
leoulendryas <leoulendryas@gmail.com>
2025-03-20 22:08:24 +0300

    Fix build.gradle.kts: Move classpath to buildscript and fix syntax

---
+++ android/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.application") version "8.7.0" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.set(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.set(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
+
+// Add this inside buildscript block
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.2") // ✅ Correct syntax for Kotlin DSL
+    }
+}
