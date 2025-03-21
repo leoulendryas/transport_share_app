f1b9b05c2ec70344ea9ce29a0c134a4ba7bb224b
leoulendryas <leoulendryas@gmail.com>
2025-03-21 22:28:02 +0300

    Update ride.dart

--- lib/models/ride.dart
+++ lib/models/ride.dart
@@ -1,10 +1,12 @@
 class Ride {
+   final String id; 
   final String from;
   final String to;
   final int seats;
-  final DateTime time;
+  final String time;
 
   Ride({
+    required this.id,
     required this.from,
     required this.to,
     required this.seats,
@@ -14,20 +16,22 @@ class Ride {
   // Factory constructor to create a Ride object from JSON
   factory Ride.fromJson(Map<String, dynamic> json) {
     return Ride(
+      id: json['id'],
       from: json['from'],
       to: json['to'],
       seats: json['seats'],
-      time: DateTime.parse(json['time']),
+      time: json['time'],
     );
   }
 
   // Method to convert a Ride object to JSON
   Map<String, dynamic> toJson() {
     return {
+      'id': id,
       'from': from,
       'to': to,
       'seats': seats,
-      'time': time.toIso8601String(),
+      'time': time,
     };
   }
 }
