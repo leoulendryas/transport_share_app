b4ca77a9af22d7d0af7e3d65b7e09dd46bb8e7fe
leoulendryas <leoulendryas@gmail.com>
2025-03-25 22:28:31 +0300

    Add app.dart

---
+++ lib/app.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'screens/auth/login_screen.dart';
+import 'screens/auth/register_screen.dart';
+import 'screens/rides/ride_list_screen.dart';
+import 'screens/rides/create_ride_screen.dart';
+import 'screens/sos/sos_screen.dart';
+import 'services/auth_service.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ride Share',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: 'Roboto',
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const AuthWrapper(),
+        '/rides': (context) => const RideListScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/register': (context) => const RegisterScreen(),
+        '/create-ride': (context) => const CreateRideScreen(),
+        '/sos': (context) => const SosScreen(rideId: 'defaultRideId'), // Add rideId
+      },
+    );
+  }
+}
+
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final authService = Provider.of<AuthService>(context);
+    return authService.isAuthenticated 
+        ? const RideListScreen()
+        : const LoginScreen();
+  }
+}
