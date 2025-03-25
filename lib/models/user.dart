3cf271236a540e673077b5c3aa0871f13803c614
leoulendryas <leoulendryas@gmail.com>
2025-03-25 22:28:31 +0300

    Add user model

---
+++ lib/models/user.dart
@@ -0,0 +1,19 @@
+class User {
+  final String id;
+  final String email;
+  final DateTime createdAt;
+
+  User({
+    required this.id,
+    required this.email,
+    required this.createdAt,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'].toString(),
+      email: json['email'],
+      createdAt: DateTime.parse(json['created_at']),
+    );
+  }
+}
