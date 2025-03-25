0fb0de2df52af4b6195fed10f262090b50549060
leoulendryas <leoulendryas@gmail.com>
2025-03-25 22:28:30 +0300

    Update Android build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application") version "8.7.0" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 allprojects {
@@ -23,11 +22,4 @@ subprojects {
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
-}
-
-// Add this inside buildscript block
-buildscript {
-    dependencies {
-        classpath("com.google.gms:google-services:4.4.2") // ✅ Correct syntax for Kotlin DSL
-    }
 }
